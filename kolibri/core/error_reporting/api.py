ac152de383df43d6f311958efd2b84bd49e94474
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-03-30 18:47:57 +0530

    Change api to use new db_utils

--- kolibri/core/error_reporting/api.py
+++ kolibri/core/error_reporting/api.py
@@ -1,18 +1,34 @@
 """
 This will be place where API will write the error to DB
 """
-from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# from .models import ErrorReport #Commenting so precommit wont cause any problem
+from .db_utils import insert_or_update_error
 
 
-class ErrorViewSet(viewsets.ModelViewSet):
+class ErrorReportAPIView(APIView):
     """
     TO DO:
-    1) Implement Serializers
+    1) Implement some sort of validator like a
     2) Implement Permissions
     3) Implement Filters
     4) Implement Write Method that will write into DB
     """
 
-    ...
+    def post(self, request):
+        try:
+            error_message = request.data.get("error_message", "")
+            traceback_info = request.data.get("traceback", "")
+
+            insert_or_update_error("FRONTEND", error_message, traceback_info)
+
+            return Response(
+                {"message": "Error report added successfully"},
+                status=status.HTTP_201_CREATED,
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
